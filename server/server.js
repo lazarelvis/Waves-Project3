82f11182985c6b98078942795fb5385a52a0e6d7
Lazar Elvis <lazarelvis15@gmail.com>
2020-07-22 21:55:48 +0300

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express"); //crud operations
+const bodyParser = require("body-parser"); //get information from request
+const cookieParser = require("cookie-parser"); //read cookie when get a request
+
+const app = express();
+const mongoose = require("mongoose"); //object modeling for node.js OMT
+require("dotenv").config(); //by "dotenv": "^6.0.0",
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const port = process.env.PORT || 3002;
+
+app.listen(port, () => {
+  console.log(`server is running ${port}`);
+});
