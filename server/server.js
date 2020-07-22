477765bf2ac447e8f7a4b65715282ccf49e72e2c
Lazar Elvis <lazarelvis15@gmail.com>
2020-07-22 22:04:27 +0300

    another change

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cookieParser = require("cookie-parser"); //read cookie when get a request
 
 const app = express();
 const mongoose = require("mongoose"); //object modeling for node.js OMT
-require("dotenv").config(); //by "dotenv": "^6.0.0",,,
+require("dotenv").config(); //by "dotenv": "^6.0.0"
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASE);
