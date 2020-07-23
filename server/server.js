3566798747f2ad1dabc37b9ee23fef215f5b5ba3
Lazar Elvis <lazarelvis15@gmail.com>
2020-07-23 14:39:23 +0300

    solved Register user, hasing password, login and creating tokens, auth route

--- server/server.js
+++ server/server.js
@@ -13,20 +13,62 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-//Models
+//midleware -> bodyparser
 
-const { User } = require("./models/user.js");
+//Models
+const { User } = require("./models/user");
+
+//Middleware
+const { auth } = require("./middleware/auth");
 
 //========== USER ===========
 
-app.post("api/users/register", (req, res) => {
-  const user = new User(req.body); //body from postman?
+app.get("/api/users/auth", auth, (req, res) => {
+  res.status(200).json({
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    car: req.user.cart,
+    history: req.user.history,
+  });
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body); //middleware  is converting to json and get back
 
   user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
     res.status(200).json({
       success: true,
-      userdata: doc,
+    });
+  });
+});
+
+app.post("/api/users/login", (req, res) => {
+  //find email
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found",
+      });
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      //isMatch true sau false
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "Wrong password" });
+
+      //generate token
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res.cookie("w_auth", user.token).status(200).json({
+          loginSuccess: true,
+        });
+      });
     });
   });
 });
